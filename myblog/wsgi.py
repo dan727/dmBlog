674d29db65b4271673be6ac415e8cf3ce93ea1ee
Daniel Monarrez <dmonar2@uic.edu>
2019-11-27 20:01:48 -0600

    Refresh

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+from django.core.wsgi import get_wsgi_application
 import os
 
-from django.core.wsgi import get_wsgi_application
+rom whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
 
