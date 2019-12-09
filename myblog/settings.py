9dfad8e9b9363f895897862dcdbdc7dc541d2653
Daniel Monarrez <dmonar2@uic.edu>
2019-12-08 18:17:51 -0600

    Refresh

--- myblog/settings.py
+++ myblog/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Danm',
-        'USER': 'Daniel',
+        'NAME': 'danm',
+        'USER': 'daniel',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
