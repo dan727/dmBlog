48908176df9c322e11fb85e35606fae6f67d7d5c
Daniel Monarrez <dmonar2@uic.edu>
2019-12-13 01:39:36 -0600

    Refresh

--- myblog/settings.py
+++ myblog/settings.py
@@ -132,5 +132,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    #os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
