18bef78354ea68161490fdd81db449c25a75a44b
Daniel Monarrez <dmonar2@uic.edu>
2020-01-16 22:34:26 -0600

    Refresh

--- myblog/settings.py
+++ myblog/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'blog/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog/static'),
+    os.path.join(BASE_DIR, 'myblog/static'),
 ]
